832507aa4cf06df59c696b37ccc2ae14f755733f
varad-comrad <fabricio_asfora_002@hotmail.com>
2024-02-02 12:40:20 -0300

    double check algorithm to find issue in convergence speed

--- src/main/java/genetic/algorithm/BaseGeneticAlgorithm.java
+++ src/main/java/genetic/algorithm/BaseGeneticAlgorithm.java
@@ -1,14 +1,12 @@
 package genetic.algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class BaseGeneticAlgorithm implements IGA {
-
+public class BaseGeneticAlgorithm{
+    String[] optionsSelectionAlgorithms = {"roulette", "tournament", "rank", "random"};
+    String[] optionsCrossoverAlgorithms = {"one-point", "two-point", "uniform"};
     private final Function<Chromosome, Float> fitnessFunction;
     private final Float elitismRate;
     private List<Boolean> uniformCrossoverMask;
@@ -23,6 +21,11 @@ public class BaseGeneticAlgorithm implements IGA {
     private final Integer chromosomeSize;
     private final Integer maxGenerations;
     private Integer generation;
+
+
+    public BaseGeneticAlgorithm(){
+        this(1000, 1000, 1000, 0.1f, 0.1f, 0.1f, 0.7f, t -> t.getState().stream().reduce(0, Integer::sum).floatValue(), "roulette");
+    }
 
 
     public BaseGeneticAlgorithm(Integer populationSize,
@@ -53,90 +56,74 @@ public class BaseGeneticAlgorithm implements IGA {
 
     }
 
-    @Override
     public List<List<Chromosome>> getAllPopulations() {
         return populations;
     }
 
-    @Override
     public Function<Chromosome, Float> fitnessFunction() {
         return this.fitnessFunction;
     }
 
-    @Override
     public Float getMutationRate() {
         return mutationRate;
     }
 
-    @Override
     public Float getProbabilityOfMutation() {
         return probabilityOfMutation;
     }
 
-    @Override
     public Float getElitismRate() {
         return elitismRate;
     }
 
-    @Override
     public Float getCrossoverRate() {
         return crossoverRate;
     }
 
-    @Override
     public Integer getPopulationSize() {
         return populationSize;
     }
 
-    @Override
     public Integer getChromosomeSize() {
         return chromosomeSize;
     }
 
-    @Override
     public Integer getMaxGenerations() {
         return maxGenerations;
     }
 
-    @Override
     public Integer getGeneration() {
         return generation;
     }
 
-    @Override
     public String getSelectionAlgorithm() {
         return this.selectionAlgorithm;
     }
 
-    @Override
     public void setSelectionAlgorithm(String selectionAlgorithm) {
         if (Arrays.stream(optionsSelectionAlgorithms).anyMatch(Predicate.isEqual(selectionAlgorithm))) {
             this.selectionAlgorithm = selectionAlgorithm;
         }
     }
-    @Override
+
     public String getCrossoverAlgorithm() {
         return crossoverAlgorithm;
     }
 
-    @Override
     public void setCrossoverAlgorithm(String crossoverAlgorithm) {
         if (Arrays.stream(optionsCrossoverAlgorithms).anyMatch(Predicate.isEqual(crossoverAlgorithm))) {
             this.crossoverAlgorithm = crossoverAlgorithm;
         }
     }
 
-    @Override
     public void setUniformCrossoverMask(List<Boolean> mask){
         this.uniformCrossoverMask = mask;
     }
 
-    @Override
     public List<Boolean> getUniformCrossoverMask(){
         return this.uniformCrossoverMask;
     }
 
-    @Override
     public void next(boolean verbose){
         if(generation == 0){
             generateInitialPopulation();
@@ -152,7 +139,176 @@ public class BaseGeneticAlgorithm implements IGA {
         addNonElites(nonElites);
     }
 
-    @Override
+    List<Chromosome> getPopulation(){
+        if(!getAllPopulations().isEmpty()){
+            return getAllPopulations().get(getGeneration());
+        }
+        throw new RuntimeException("Population not initialized");
+    }
+    Integer getEliteNumber(){
+        return (int) (getPopulationSize() * getElitismRate());
+    }
+
+    Integer getNonEliteNumber(){
+        return getPopulationSize() - getEliteNumber();
+    }
+
+    void generateInitialPopulation(){
+        getAllPopulations().add(new ArrayList<>());
+        getAllPopulations().get(getGeneration()).addAll(Chromosome.generatePopulation(getPopulationSize(), getChromosomeSize(), fitnessFunction()));
+    }
+
+    void evaluateChromosomes(boolean verbose){
+        getAllPopulations().get(getGeneration()).stream().parallel().forEach(Chromosome::computeFitness);
+        if(verbose){
+            for(int i = 0; i<getPopulationSize(); i++){
+                getAllPopulations().get(getGeneration()).get(i).printFitness(i);
+            }
+        }
+    }
+
+    void elitifyChromosomes(){
+        List<Chromosome> aux = getAllPopulations().get(getPreviousGeneration()).stream().sorted(Comparator.comparing(Chromosome::getFitness)).toList();
+        getAllPopulations().add(new ArrayList<>(aux.subList(getPopulationSize()-getEliteNumber(), getPopulationSize())));
+    }
+
+    int getPreviousGeneration() {
+        return getGeneration() - 1;
+    }
+
+    void preProcessRoulette(){
+        Float sum = getAllPopulations().get(getPreviousGeneration()).stream().reduce(0f, (acc, c) -> {
+            c.setRouletteValue(acc + c.getFitness());
+            return acc + c.getFitness();
+        }, Float::sum);
+        getAllPopulations().get(getPreviousGeneration()).stream().parallel().forEach(c -> c.setRouletteValue(c.getRouletteValue()/sum));
+    }
+
+    List<Chromosome> roulette(Integer numberOfChromosomes) throws VerifyError{
+        preProcessRoulette();
+        List<Chromosome> res = Collections.synchronizedList(new ArrayList<>());
+        List<Thread> threads = new ArrayList<>();
+        for(int i=0; i<numberOfChromosomes; i++){
+            Thread worker = new Thread(() -> {
+                List<Chromosome> aux = getAllPopulations()
+                        .get(getPreviousGeneration())
+                        .stream()
+                        .parallel()
+                        .filter(c -> c.getRouletteValue() >= Math.random())
+                        .toList();
+
+                Optional<Chromosome> chromosome = aux
+                        .stream()
+                        .min(Comparator.comparing(Chromosome::getRouletteValue));
+
+                if(chromosome.isPresent()){
+                    res.add(chromosome.get());
+                } else{
+                    throw new VerifyError("Unexpected error occurred");
+                }
+            });
+            threads.add(worker);
+            worker.start();
+        }
+        for(var thread: threads){
+            try{
+                thread.join();
+            } catch(InterruptedException ignored){
+            }
+        }
+        return res;
+    }
+
+    List<Chromosome> tournament(Integer numberOfChromosomes){
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        return null;
+    }
+
+    List<Chromosome> rank(Integer numberOfChromosomes){
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        return null;
+    }
+
+    List<Chromosome> random(Integer numberOfChromosomes){
+        List<Chromosome> aux = new ArrayList<>(getPopulationSize());
+        for(int i=0; i<numberOfChromosomes;i++){
+            aux.set(i, getAllPopulations().get(getPreviousGeneration()).get(Math.floorMod(getPopulationSize(),getPopulationSize())));
+        }
+        return aux;
+    }
+
+
+    List<Chromosome> selectChromosomes(){
+        switch (getSelectionAlgorithm()) {
+            case "roulette" -> {
+                return roulette(getNonEliteNumber());
+            }
+            case "tournament" -> {
+                return tournament(getNonEliteNumber());
+            }
+            case "rank" -> {
+                return rank(getNonEliteNumber());
+            }
+            case "random" -> {
+                return random(getNonEliteNumber());
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+
+    void crossoverChromosomes(List<Chromosome> nonElites){
+        List<Thread> threads = new ArrayList<>();
+        for(int i=0; i<(int)(getPopulationSize()*getCrossoverRate());i++){
+            Thread actor = new Thread(() -> {
+                Random rand = new Random();
+                int i1 = Math.floorMod(rand.nextInt(), nonElites.size());
+                int i2 = Math.floorMod(rand.nextInt(), nonElites.size());
+                while(i2 == i1){
+                    i2 = Math.floorMod(rand.nextInt(), nonElites.size());
+                }
+                Chromosome[] children = nonElites.get(i1).crossover(nonElites.get(i2), getCrossoverAlgorithm());
+                nonElites.set(i1, children[0]);
+                nonElites.set(i2, children[1]);
+            });
+            threads.add(actor);
+            actor.start();
+        }
+
+        for(var thread: threads){
+            try{
+                thread.join();
+            } catch(InterruptedException ignored){
+            }
+        }
+    }
+
+    void mutateChromosomes(List<Chromosome> nonElites){
+        nonElites.stream().parallel().forEach(c -> {
+            if (Math.random() < getMutationRate()){
+                c.mutate(getProbabilityOfMutation());
+            }
+        });
+    }
+
+    void addNonElites(List<Chromosome> nonElites){
+        getPopulation().addAll(nonElites);
+    }
+
+    List<Chromosome> getBestChromosomes() throws NoSuchElementException {
+        return getAllPopulations().stream()
+                .map(arr -> {
+                    var res = arr.stream().max(Comparator.comparing(Chromosome::getFitness));
+                    if(res.isPresent()){
+                        return res.get();
+                    } else{
+                        throw new NoSuchElementException("Unexpected error occurred");
+                    }
+                }).toList();
+    }
+
     public void run(boolean verbose){
         while (generation < getMaxGenerations()){
             next(verbose);
--- src/main/java/genetic/algorithm/Main.java
+++ src/main/java/genetic/algorithm/Main.java
@@ -1,33 +1,19 @@
 package genetic.algorithm;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.function.Function;
 
-class Aux{
-    public Float val = 0f;
-    public Float fitness;
-    public Float getFitness(){
-        return fitness;
-    }
 
-    public Aux(Float fitness){
-        this.fitness = fitness;
-    }
-}
 public class Main {
 
     public static void main(String[] args) {
-        Integer populationSize = 100;
-        Integer chromosomeSize = 100;
-        Integer maxGenerations = 100;
+        Integer populationSize = 1000;
+        Integer chromosomeSize = 1000;
+        Integer maxGenerations = 200;
         Float elitismRate = 0.1f;
         Float mutationRate = 0.1f;
-        Float probabilityMutation = 0.1f;
-        Float crossoverRate = 0.5f;
-        Function<Chromosome, Float> fitnessFunction = (Function<Chromosome, Float>) t -> t.getState().stream().reduce(0, Integer::sum).floatValue();
+        Float probabilityMutation = 0.2f;
+        Float crossoverRate = 0.8f;
+        Function<Chromosome, Float> fitnessFunction = t -> t.getState().stream().reduce(0, Integer::sum).floatValue();
         String selectionAlgorithm = "roulette";
         BaseGeneticAlgorithm ga = new BaseGeneticAlgorithm(
                 populationSize,
@@ -41,6 +27,8 @@ public class Main {
                 selectionAlgorithm
         );
         ga.run(false);
-        ga.getBestFromEachGeneration().forEach(c -> c.printFitness(ga.getBestFromEachGeneration().indexOf(c)));
+        var aux = ga.getBestFromEachGeneration();
+        for(int i=0; i<aux.size(); i++)
+            aux.get(i).printFitness(i);
     }
 }
